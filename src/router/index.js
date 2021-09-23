78055356faecf69468d100bab85348b04d35eb78
maochenxi <2501767678@qq.com>
2021-09-23 18:30:37 +0800

    完成用户列表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome'
+import Users from '../components/users/Users'
 
 Vue.use(VueRouter)
 
@@ -9,7 +11,15 @@ const routes = [
   // 如果用户在/这个地址，就重定向到/login
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users }
+    ]
+  }
 ]
 
 const router = new VueRouter({
